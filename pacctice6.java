41b3fd6b92b58073b87356a9c1f07dec022f2cbb
sidorenkoso <sidorenkosofia540@gmail.com>
2024-03-08 16:23:14 +0200

    progress at the end of practice

--- pacctice6.java
+++ pacctice6.java
@@ -9,6 +9,30 @@ class pactice6{
         }
         System.out.println("Введіть підрядок для знаходження: ");
         String find = args[0];
-        
-    }    
+        result[] results = new result[countlines];
+        for(int i=0; i<countlines; i++){
+            String line = lines[i];
+            int findings = Countfindings(line, find);
+            results[i] = new result(findings, i);
+        }
+    }
+
+    private static int Countfindings(String text, String pattern){
+        int count = 0;
+        int index = 0;
+        while((index = text.indexOf(pattern, index))!= -1){
+            count++;
+            index+=pattern.length();
+        }
+        return count;
+    }
+
+    static class result {
+        int index;
+        int numberOffindinig;
+        public result(int index, int numberOffinding){
+            this.index = index;
+            this.numberOffindinig = numberOffinding;
+        }
+    }   
 }
